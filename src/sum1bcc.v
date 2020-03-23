bd70cdb779e7df08e0610f5656b1601bc2e55fde
jcrojasda <55933743+jcrojasda@users.noreply.github.com>
2020-03-23 15:50:50 -0500

    Update sum1bcc.v

--- src/sum1bcc.v
+++ src/sum1bcc.v
@@ -1,4 +1,4 @@
-module sum1bcc (A, B, Ci,Cout,S);
+module sum1bcc (A, B, Ci,Cout,S); //se crea el sumador y se dan las variables que se van a usar
 
   input  A;
   input  B;
@@ -6,12 +6,14 @@ module sum1bcc (A, B, Ci,Cout,S);
   output Cout;
   output S;
 
-  reg [1:0] st;
-  assign S = st[0];
-  assign Cout = st[1];
+  //se definen las variables que entran (input) y las variables que salen (output)
+  
+  reg [1:0] st; //se crea un arreglo de dos espacios para poner el resultado de la suma que puede requerir 2 bits
+  assign S = st[0]; //El primer dígito va en el espacio 0 y se asigna a la salida S
+  assign Cout = st[1]; //El segundo dígito va en el espacio 1 y se asigna a la salida Cout
 
-  always @ ( * ) begin
-  	st  = 	A+B+Ci;
+  always @ ( * ) begin //el programa siempre va a estar pendiente de la entrada
+  	st  = 	A+B+Ci; //se hace la suma de A+B+Ci y se ponen los dos dígitos en st
   end
   
 endmodule
